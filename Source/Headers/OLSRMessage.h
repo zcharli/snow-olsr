fc0d0352891cadd2ab416a4f5364568fee314314
Kyle <kyle.thompson228@gmail.com>
2016-04-09 15:34:53 -0400

    Began adding message serialization.

--- Source/Headers/OLSRMessage.h
+++ Source/Headers/OLSRMessage.h
@@ -1,13 +1,35 @@
 #ifndef OLSR_MESSAGE_H
 #define OLSR_MESSAGE_H
 
-using namespace std;
+#include <vector>
+
+#include "Message.h"
 
 class OLSRMessage
 {
 public:
 	OLSRMessage();
 	~OLSRMessage();
+    
+    char[] serialize();
+
+private:
+    
+    class MessageHeader
+    {
+    public:
+        MessageHeader(Message&);
+        unsigned char type;
+        unsigned char vtime;
+        unsigned short messageSize;
+        unsigned int originatorAddress;
+        unsigned char timeToLive;
+        unsigned char hopCount;
+        unsigned short messageSequenceNumber;
+        char[] message;
+        
+        char[] serialize();
+    }
 	
 };
 
